08cd3b218fbb087f45470bb756ac4aba658d0099
Tristan-Fullmer <ful15007@byui.edu>
2021-02-19 15:37:47 -0600

    Log message when report is printed

--- nel_reports/models/models.py
+++ nel_reports/models/models.py
@@ -1,15 +1,24 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import api, models
+import logging
+import datetime
+_logger = logging.getLogger(__name__)
 
-# class nel_reports(models.Model):
-#     _name = 'nel_reports.nel_reports'
+class ParticularReport(models.AbstractModel):
+    _name = 'report.studio_customization.studio_report_docume_89133f85-1132-4e8b-af71-de75d2c4c06a'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    @api.model
+    def _get_report_values(self, docids, data=None):
+        report_obj = self.env['ir.actions.report']
+        report = report_obj._get_report_from_name('studio_customization.studio_report_docume_89133f85-1132-4e8b-af71-de75d2c4c06a')
+        user_name = self.env['res.user'].browse(self.env.user).name
+        _logger.debug('Work Order Routing Information Report printed at %s by %s', str(datetime.datetime.today()), user_name)
+
+        docargs = {
+            'doc_ids': docids,
+            'doc_model': report.model,
+            'docs': self,
+        }
+        # Run some code
+        return docargs
